43f2b0b5d9cdd3bd930a186f922d8a895f31aef8
王帅 <m15811024346@163.com>
2019-12-10 17:20:36 +0800

    上传今天学习的课程内容。time：--2019-12-10

--- struct_interFace/main.go
+++ struct_interFace/main.go
@@ -1,6 +1,31 @@
 package main
 
 import "fmt"
+
+//同一个结构体可以实现多个接口
+//接口嵌套
+type animal interface {
+	mover
+	eater
+}
+
+type mover interface {
+	move()
+}
+type eater interface {
+	eat(string)
+}
+//cat实现了mover和eater这两个接口
+type cat struct {
+	name string
+	feet int8
+}
+func (c cat) eat(food string){
+	fmt.Printf("猫吃%s～～～",food)
+}
+func (c cat)move(){
+	fmt.Println("走猫步～～")
+}
 func main(){
 	fmt.Println("hello world")
 
---
+++ type_assin/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//类型断言
+//类型断言是为了了解空接口中接收的值是什么类型。。
+func assign(a interface{}){
+	fmt.Printf("%T\n",a)
+	str,ok := a.(string)
+	if !ok{
+		fmt.Println("猜错了")
+	}else {
+		fmt.Printf("传进来的是个字符串\n",str)
+	}
+}
+func assign2(a interface{}){
+	fmt.Printf("%T\n",a)
+	switch t:=a.(type) {
+	case string:
+		fmt.Println("是一个字符串",t)
+	case int64:
+		fmt.Println("是一个int64",t)
+	case bool:
+		fmt.Println("是一个bool",t)
+	case int:
+		fmt.Println("是一个int",t)
+	}
+}
+
+func main(){
+	assign("哈哈哈" )
+	assign2("嘿嘿嘿")
+	assign2(true)
+	assign2(1111)
+}
